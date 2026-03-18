6ccb25c1b229cee791a48667a12b25515ad3de05
Melisse Deocareza <imelissegd@gmail.com>
2026-03-18 14:06:25 +0800

    Remove unused java.awt and java.net.URI imports

--- app/src/main/java/com/mgd/employee_mgmt/EmployeeManagementApp.java
+++ app/src/main/java/com/mgd/employee_mgmt/EmployeeManagementApp.java
@@ -3,15 +3,10 @@ package com.mgd.employee_mgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-import java.net.URI;
-
 @SpringBootApplication
 public class EmployeeManagementApp {
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee Management System by Mel!");
-
         SpringApplication.run(EmployeeManagementApp.class, args);
-
     }
 }
